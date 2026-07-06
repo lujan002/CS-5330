be31a51336857986d2e803f1ed78dc500317a058
Luke Jansen <20JansenLu@gmail.com>
2026-07-06 19:14:25 -0400

    working model

---
+++ project4/build/CMakeFiles/project4.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/media/ubuntu-22/San Disk 2TB/Northeastern Courses/CS 5330/project4/src/gl_renderer.cpp" "CMakeFiles/project4.dir/src/gl_renderer.cpp.o" "gcc" "CMakeFiles/project4.dir/src/gl_renderer.cpp.o.d"
+  "/media/ubuntu-22/San Disk 2TB/Northeastern Courses/CS 5330/project4/src/main.cpp" "CMakeFiles/project4.dir/src/main.cpp.o" "gcc" "CMakeFiles/project4.dir/src/main.cpp.o.d"
+  "/media/ubuntu-22/San Disk 2TB/Northeastern Courses/CS 5330/project4/src/obj_loader.cpp" "CMakeFiles/project4.dir/src/obj_loader.cpp.o" "gcc" "CMakeFiles/project4.dir/src/obj_loader.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project4/build/CMakeFiles/project4.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project4.dir/src/gl_renderer.cpp.o"
+  "CMakeFiles/project4.dir/src/gl_renderer.cpp.o.d"
+  "CMakeFiles/project4.dir/src/main.cpp.o"
+  "CMakeFiles/project4.dir/src/main.cpp.o.d"
+  "CMakeFiles/project4.dir/src/obj_loader.cpp.o"
+  "CMakeFiles/project4.dir/src/obj_loader.cpp.o.d"
+  "project4"
+  "project4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
