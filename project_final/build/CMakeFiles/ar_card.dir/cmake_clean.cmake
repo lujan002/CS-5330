ef8e6343ed6389071d45c92345d0f93f6f23c494
Luke Jansen <20JansenLu@gmail.com>
2026-07-24 15:26:26 -0400

    added final project, using Canny + findContours

---
+++ project_final/build/CMakeFiles/ar_card.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ar_card.dir/data/models/main.cpp.o"
+  "CMakeFiles/ar_card.dir/data/models/main.cpp.o.d"
+  "ar_card"
+  "ar_card.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ar_card.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ project_final/build/CMakeFiles/ar_card.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ar_card.
+# This may be replaced when dependencies are built.
